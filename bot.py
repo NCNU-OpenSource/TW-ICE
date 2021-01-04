81966469858937ee1e1a02f6699bb074379021e4
ChengEn <45934743+peter6098790@users.noreply.github.com>
2021-01-04 17:28:00 +0800

    add new feature: new item notify

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import telepot
 import time
 from telepot.loop import MessageLoop
 from pprint import pprint
-from telepot.namedtuple import InlineKeyboardMarkup as InlineMarkup
-from telepot.namedtuple import InlineKeyboardButton as InlineBtn
+from telepot.namedtuple import InlineKeyboardMarkup 
+from telepot.namedtuple import InlineKeyboardButton 
 from telepot.namedtuple import ReplyKeyboardMarkup as ReplyMarkup
 from telepot.namedtuple import KeyboardButton as Btn
 import DB_CRUD as db
@@ -26,40 +26,46 @@ def getItemList():
         itemString =  "There is Nothing in the refrigerator :("
     return itemString
 
-# def getNewItem():
-#     url = BASE_DIR + "\\villagerlimits.PNG"
-#     inlineBtns = [
-#                     [[InlineKeyboardButton(text="btn1",callback_data='I have Nothing To do'), InlineKeyboardButton(text="btn2",callback_data='0'),InlineKeyboardButton(text="btn3",callback_data='0'), InlineKeyboardButton(text="btn4",callback_data='0')]
-#                 ]
-#     bot.sendPhoto("781745255", photo=open(url, 'rb'), reply_markup = InlineKeyboardMarkup(inline_keyboard= inlineBtns))
+# def updateItem(barcode,itemname,expirationDate)
+#     update_data_use_serial_number(1,itemname,barcode)
+#     update_data_use_serial_number((5,itemname,barcode))
+
+# wait for the part of web cam
+def getNewItem(chat_id_list,barcode,url):
+    openurl = BASE_DIR + url
+    for i in chat_id_list:
+        bot.sendPhoto(i, photo=open(openurl, 'rb'), caption = 'There is a new item: ' + barcode + ' !')
 
 def on_chat_message(msg):
     global itemString,chat_id
     content_type, chat_type, chat_id = telepot.glance(msg)
+    ## 
+    inputdata = msg['text'].split() 
     if content_type == 'text':
         bot.sendMessage(chat_id, msg['text'])
-        pprint(msg)
+        #pprint(msg)
         chat_id = msg['chat']['id']
         print(chat_id)
         from_id = msg['from']['id']
-        if msg['text'] == '/help' :
+        if inputdata[0] == '/help' :
             replyBtns = [
                     [Btn(text='/show')],
-                    [Btn(text='/add'), Btn(text='/update'), Btn(text='/vol_up')],
+                    [Btn(text='/add'), Btn(text='/updateInfo'), Btn(text='/delete')],
+                    [Btn(text='/Immediate item'), Btn(text='/Expiring item')]
                 ]
             time.sleep(1)
             bot.sendMessage(chat_id, 'Here\'s all function buttons', reply_markup=ReplyMarkup(keyboard=replyBtns))
 
-        elif msg['text'] == '/show' :
+        elif inputdata[0] == '/show' :
             test = ''
             test = getItemList()
             print(type(test))
             print(test)
             bot.sendMessage(chat_id, test)
-        elif msg['text'] == '/update' :
-            url = BASE_DIR + "\\villagerlimits.PNG"
-            bot.sendPhoto(chat_id, photo=open(url, 'rb'))
-        elif msg['text'] == '/add'  :
+        elif inputdata[0] == '/updateInfo' :
+            #if(len(inputdata)<3)
+                bot.sendMessage(chat_id, "3")
+        elif inputdata[0] == '/add'  :
             bot.sendMessage(chat_id, "3")
 
 if __name__ == '__main__':
@@ -68,32 +74,27 @@ if __name__ == '__main__':
     with open(os.path.join(BASE_DIR, 'token.txt')) as f:
         TELEGRAM_BOT_TOKEN = f.read().strip() # Telegram Bot Token
     bot = telepot.Bot(TELEGRAM_BOT_TOKEN)
-    #bot.sendMessage("781745255", "hello")
     print("I'm listening...")
+    testurl = "\\villagerlimits.PNG"
+    barcode = 'test0999'
+    chat_id_list = {'217724690'} #'781745255',
+    getNewItem(chat_id_list,barcode,testurl)
 
     # global variables
     totalList = db.read_all_data()
-    #print(totalList)
     itemString = '條碼號 品名' + '\n'
-    chat_id=''
-    getNewItem()
+   
+    #MessageLoop(bot, {'chat': on_chat_message, 'callback_query': on_callback_query}).run_as_thread()
     MessageLoop(bot,on_chat_message).run_as_thread()
     while 1:
         time.sleep(10)
 
 
 
-# def updateItemName()
 
-# def updateItemExpirationDate()
-
-    #text = msg['text']
-    #bot.sendMessage(chat_id, text)
-
-    #photo = 'https://imgur.com/gallery/UoGUPux'
-    #bot.sendPhoto(chat_id, photo)
-
-
+# url = BASE_DIR + "\\villagerlimits.PNG"
+            # barcode = 'test0999'
+            # bot.sendPhoto(chat_id, photo=open(url, 'rb'), caption = 'There is a new item: ' + barcode + ' !')
 
 
 
