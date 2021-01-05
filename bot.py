66daca7069ac71cf51acd4a273210d8ae0ddc956
ChengEn <45934743+peter6098790@users.noreply.github.com>
2021-01-05 23:30:44 +0800

    Add files via upload

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ import mysql.connector ,time ,os
 from mysql.connector import Error
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
-# show all items in the refrigerator
+# 列出所有在冰箱中的食物
 def getItemList():
     global totalList, itemString
     totalList = db.read_data_in_ref()
@@ -20,48 +20,46 @@ def getItemList():
     if totalList:
         for item in totalList:
             #tmp = item  # tmp[0]: pk, tmp[1]: qrcode, tmp[2]: item name, tmp[6]: expire_date
-            #print(tmp)
-            # if item without item name
+            # 如果食物還沒有設定品名
             if item[2] == None:
                 itemString = itemString + item[1] + '\t' + 'None ' + '\t' + str(item[6]) + '\n'
             else:
                 itemString = itemString + item[1] + '\t' + item[2]  + '\t' + str(item[6]) + '\n'
-    # there is nothing
+    #冰箱中沒東西
     else:
         itemString =  "There is Nothing in the refrigerator :("
     return itemString
 
+# 更新食品資訊(食物名稱,保存期限)
 def updateItem(qrcode,itemname,expirationDate):
     db.update_data_use_serial_number(1,itemname,qrcode)
     db.update_data_use_serial_number(5,expirationDate,qrcode)
 
-# wait for the part of web cam
+# 食品被拿出時推播通知使用者
 def takeOffItem(chat_id_list,qrcode,url):
     openurl = BASE_DIR + url
     for i in chat_id_list:
         bot.sendPhoto(i, photo=open(openurl, 'rb'), caption = qrcode + ' has been took off!')
 
-# wait for the part of web cam
+# 新的食品被放進冰箱時推播通知
 def getNewItem(chat_id_list,qrcode,url):
     openurl = BASE_DIR + url
     for i in chat_id_list:
         bot.sendPhoto(i, photo=open(openurl, 'rb'), caption = 'There is a new item: ' + qrcode + ' !')
 
+# 監聽telegram訊息
 def on_chat_message(msg):
     global itemString,chat_id
     content_type, chat_type, chat_id = telepot.glance(msg)
-    ## 
     inputdata = msg['text'].split() 
-    #print(inputdata)
     if content_type == 'text':
         #bot.sendMessage(chat_id, msg['text'])
-        #pprint(msg)
         chat_id = msg['chat']['id']
         from_id = msg['from']['id']
-        # for demo
+        # demo用途,把現場有加機器人的都加進清單,推播時他們才看的到
         if chat_id not in chat_id_list :
             chat_id_list.append(chat_id)
-        #print(chat_id)
+        ## 使用者reply新物品推播時圖片可直接輸入名稱,保存期限(有bug,未實裝)
         # reply photo to set item name and expire_date
         # if msg['reply_to_message']:
         #     tmp = msg['reply_to_message']['caption'].split()
@@ -69,6 +67,8 @@ def on_chat_message(msg):
         #     item_name = inputdata[0]
         #     expire_date = inputdata[1]
         #     print(item_name,expire_date)
+
+        # 列出所有按鈕
         if inputdata[0] == '/help' :
             replyBtns = [
                     [Btn(text='/show')],
@@ -103,8 +103,9 @@ def on_chat_message(msg):
                 for people in chat_id_list:
                     for item in immediateList:
                         openurl = BASE_DIR + item[2]
-                        bot.sendMessage(chat_id,openurl)
-                        #bot.sendPhoto(people, photo=open(openurl, 'rb'), caption =  'Name:' + item[0] +' ' + 'CountDown:' + item[1])
+                        # 開發時測試用
+                        #bot.sendMessage(chat_id,openurl)
+                        bot.sendPhoto(people, photo=open(openurl, 'rb'), caption =  'Name:' + item[0] +' ' + 'CountDown:' + item[1])
             else :
                 bot.sendMessage(chat_id, 'No food is about to expire!')
         elif inputdata[0] == '/Expiring_item'  :
@@ -114,8 +115,8 @@ def on_chat_message(msg):
                 for people in chat_id_list:
                     for item in expiredList:
                         openurl = BASE_DIR + item[2]
-                        bot.sendMessage(chat_id,openurl)
-                        #bot.sendPhoto(people, photo=open(openurl, 'rb'), caption =  'Name:' + item[0] +' ' + 'CountDown:' + item[1])
+                        #bot.sendMessage(chat_id,openurl)
+                        bot.sendPhoto(people, photo=open(openurl, 'rb'), caption =  'Name:' + item[0] +' ' + 'CountDown:' + item[1])
             else :
                 bot.sendMessage(chat_id, 'No food is expired !')  
 
@@ -126,12 +127,12 @@ if __name__ == '__main__':
     totalList = []
     chat_id_list = []
 
-
     BASE_DIR = os.path.dirname(os.path.realpath(__file__)) # catch token
     with open(os.path.join(BASE_DIR, 'token.txt')) as f:
         TELEGRAM_BOT_TOKEN = f.read().strip() # Telegram Bot Token
     bot = telepot.Bot(TELEGRAM_BOT_TOKEN)
     print("I'm listening...")
+
     MessageLoop(bot,on_chat_message).run_as_thread()
     while 1:
         time.sleep(10)
