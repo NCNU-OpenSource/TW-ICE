1eeaef57d40a2d4459cbacd28660f35d2b51ca2d
wenwei <zxc85070523@gmail.com>
2021-01-05 11:23:44 +0800

    update picamera

--- DB_CRUD.py
+++ DB_CRUD.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from mysql.connector import Error
 import time, os
+import datetime
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 table_name = 'test_proudct'
 
@@ -14,7 +15,7 @@ def connector():
             password='lsa4')
 
         if connection.is_connected():
-            print("already connected to database")
+            #print("already connected to database")
             return connection
 
     except Error as e:
@@ -24,27 +25,27 @@ def create_new_table():
     # connect database
     connection = connector()
     mycursor = connection.cursor()
-    sql = "CREATE TABLE {} (id INT AUTO_INCREMENT PRIMARY KEY, serial_number VARCHAR(255), name VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, first_time DATETIME, recent_input_time DATETIME NULL, recent_output_time DATETIME NULL, expiration_date DATETIME NULL, photo_url VARCHAR(255));"
+    sql = "CREATE TABLE {} (id INT AUTO_INCREMENT PRIMARY KEY, serial_number VARCHAR(255), name VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, first_time DATETIME, recent_input_time DATETIME NULL, recent_output_time DATETIME NULL, expiration_date DATETIME NULL, photo_url VARCHAR(255), product_status BOOLEAN);"
     mycursor.execute( sql.format(table_name) )
     #mycursor.execute("CREATE TABLE test_proudct (id INT AUTO_INCREMENT PRIMARY KEY, serial_number VARCHAR(255), name VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL, first_time DATETIME, recent_input_time DATETIME NULL, recent_output_time DATETIME NULL, expiration_date DATETIME NULL)")
 
     if (connection.is_connected()):
         mycursor.close()
         connection.close()
-        print("Database connect closed.")
+        #print("Database connect closed.")
 
-def create_new_data_with_qrcode(qrcode_number):
+def create_new_data_with_qrcode(qrcode_number, exp, URL):
     # Get time
     t = time.localtime()
-    result = time.strftime('%Y-%m-%d %H:%M:%S',t)
-
+    result = time.strftime('%Y-%m-%d %H:%M:%S', t)
+    exp_time = datetime.datetime.now()+exp
     # connect SQL
     connection = connector()
     mycursor = connection.cursor()
 
-    sql = "INSERT INTO {} (serial_number, first_time, recent_input_time) VALUES (%s, %s, %s);"
-    new_data = (str(qrcode_number), result, result)
-    mycursor.execute(sql.format(table_name), new_data)
+    sql = "INSERT INTO {table} (serial_number, first_time, recent_input_time, expiration_date, photo_url, product_status) VALUES (%s, %s, %s, %s, %s, 1);"
+    new_data = (str(qrcode_number), result, result, exp_time, URL)
+    mycursor.execute(sql.format(table=table_name), new_data)
     connection.commit()
 
 def read_all_data():
@@ -53,8 +54,9 @@ def read_all_data():
     sql = "SELECT * FROM {}"
     mycursor.execute(sql.format(table_name))
     myresult = mycursor.fetchall()
-    for x in myresult:
-        print(x)
+    # for x in myresult:
+    #     print(x)
+    return myresult
 
 def read_specified_data_use_serial_number(serial_number):
     connection = connector()
@@ -64,8 +66,9 @@ def read_specified_data_use_serial_number(serial_number):
     number = (str(serial_number),)
     mycursor.execute(sql.format(table_name), number)
     myresult = mycursor.fetchall()
-    for x in myresult:
-        print(x)
+    # for x in myresult:
+    #     print(x)
+    return myresult
 
 def read_specified_data_use_name(name):
     connection = connector()
@@ -75,8 +78,9 @@ def read_specified_data_use_name(name):
     number = (str(name),)
     mycursor.execute(sql.format(table_name), number)
     myresult = mycursor.fetchall()
-    for x in myresult:
-        print(x)
+    # for x in myresult:
+    #     print(x)
+    return myresult
 
 def update_data_use_serial_number(update_type,update_data,serial_number):
     connection = connector()
@@ -99,6 +103,9 @@ def update_data_use_serial_number(update_type,update_data,serial_number):
     elif update_type == 6: # type 6 update photo_url
         sql = "UPDATE {} SET photo_url = %s WHERE serial_number = %s"
         val = (update_data, serial_number)
+    elif update_type == 7: # type 7 update product_status
+        sql = "UPDATE {} SET product_status = %s WHERE serial_number = %s"
+        val = (update_data, serial_number)
     else:
         print("None type is {} , please choose again.".format(update_type))
         return 0
@@ -115,3 +122,9 @@ def delete_data_use_serial_number(serial_number):
     mycursor.execute(sql.format(table_name), number)
     connection.commit()
 
+<<<<<<< Updated upstream
+=======
+# b = '0'
+# a = 'A0002'
+# update_data_use_serial_number(7,b,a)
+>>>>>>> Stashed changes
---
+++ picamera.py
@@ -0,0 +1,41 @@
+import time, os,datetime
+import DB_CRUD as db
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+def take_out(qrcode_number):
+    t = time.localtime()
+    take_out_status = '0'
+    result = time.strftime('%Y-%m-%d %H:%M:%S', t)
+    db.update_data_use_serial_number(4,result,qrcode_number)
+    db.update_data_use_serial_number(7,take_out_status,qrcode_number)
+def put_in_again(qrcode_number):
+    t = time.localtime()
+    put_in_status = '1'
+    result = time.strftime('%Y-%m-%d %H:%M:%S', t)
+    db.update_data_use_serial_number(3,result,qrcode_number)
+    db.update_data_use_serial_number(7,put_in_status,qrcode_number)
+def check_data(qrcode_number):
+    if db.read_specified_data_use_serial_number(qrcode_number): # data is exist
+        if db.read_specified_data_use_serial_number(qrcode_number)[0][8] == 1:
+            take_out(qrcode_number)
+        elif db.read_specified_data_use_serial_number(qrcode_number)[0][8] == 0:
+            put_in_again(qrcode_number)
+    else: # data is not exist
+        photoURL = BASE_DIR + '\\photo\\' + qrcode_number
+        exptime = 0
+        if qrcode_number[0] == 'A':
+            exptime = datetime.timedelta(hours=8)
+        elif qrcode_number[0] == 'B':
+            exptime = datetime.timedelta(hours=16)
+        elif qrcode_number[0] == 'C':
+            exptime = datetime.timedelta(hours=24)
+        elif qrcode_number[0] == 'D':
+            exptime = datetime.timedelta(hours=36)
+        elif qrcode_number[0] == 'E':
+            exptime = datetime.timedelta(hours=48)
+        else:
+            print('data format error')
+            return 0
+        db.create_new_data_with_qrcode(qrcode_number, exptime, photoURL)
+
+a = 'B001'
+check_data(a)
