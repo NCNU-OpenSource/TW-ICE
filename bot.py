e03157f7845100901bd23edaedb79e3f31505b80
ChengEn <45934743+peter6098790@users.noreply.github.com>
2021-01-05 21:39:33 +0800

    update

--- bot.py
+++ bot.py
@@ -16,35 +16,38 @@ BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 def getItemList():
     global totalList, itemString
     totalList = db.read_data_in_ref()
-    itemString = '條碼號 品名' + '\n'
+    itemString = 'qrcode  name  expire_date' + '\n'
     if totalList:
         for item in totalList:
-            tmp = item  # tmp[0]: pk, tmp[1]: Bar code, tmp[2]: item name
+            #tmp = item  # tmp[0]: pk, tmp[1]: qrcode, tmp[2]: item name, tmp[6]: expire_date
+            #print(tmp)
             # if item without item name
-            if tmp[2] == None:
-                itemString = itemString + tmp[1] + ' ' + 'None' + '\n'
+            if item[2] == None:
+                itemString = itemString + item[1] + '\t' + 'None ' + '\t' + str(item[6]) + '\n'
             else:
-                itemString = itemString + tmp[1] + ' ' + tmp[2] + '\n'
+                itemString = itemString + item[1] + '\t' + item[2]  + '\t' + str(item[6]) + '\n'
+            print(item[6])
+            print(type(item[6]))
     # nothing in the refrigerator
     else:
         itemString =  "There is Nothing in the refrigerator :("
     return itemString
 
-# def updateItem(barcode,itemname,expirationDate):
-#     update_data_use_serial_number(1,itemname,barcode)
-#     update_data_use_serial_number((5,itemname,barcode))
+def updateItem(qrcode,itemname,expirationDate):
+    db.update_data_use_serial_number(1,itemname,qrcode)
+    db.update_data_use_serial_number(5,expirationDate,qrcode)
 
 # wait for the part of web cam
-def takeOffItem(chat_id_list,barcode,url):
+def takeOffItem(chat_id_list,qrcode,url):
     openurl = BASE_DIR + url
     for i in chat_id_list:
-        bot.sendPhoto(i, photo=open(openurl, 'rb'), caption = barcode + ' has been took off!')
+        bot.sendPhoto(i, photo=open(openurl, 'rb'), caption = qrcode + ' has been took off!')
 
 # wait for the part of web cam
-def getNewItem(chat_id_list,barcode,url):
+def getNewItem(chat_id_list,qrcode,url):
     openurl = BASE_DIR + url
     for i in chat_id_list:
-        bot.sendPhoto(i, photo=open(openurl, 'rb'), caption = 'There is a new item: ' + barcode + ' !')
+        bot.sendPhoto(i, photo=open(openurl, 'rb'), caption = 'There is a new item: ' + qrcode + ' !')
 
 def on_chat_message(msg):
     global itemString,chat_id
@@ -53,52 +56,53 @@ def on_chat_message(msg):
     inputdata = msg['text'].split() 
     print(inputdata)
     if content_type == 'text':
-        bot.sendMessage(chat_id, msg['text'])
-        #pprint(msg)
+        #bot.sendMessage(chat_id, msg['text'])
+        pprint(msg)
         chat_id = msg['chat']['id']
-        print(chat_id)
         from_id = msg['from']['id']
+        # demo用途,把不在清單內的用戶加入清單,新食品進入時一起推播
+        if chat_id not in chat_id_list :
+            chat_id_list.append(chat_id)
+        #print(chat_id)
+        # reply photo to set item name and expire_date
+        # if msg['reply_to_message']:
+        #     tmp = msg['reply_to_message']['caption'].split()
+        #     qrcode_number = tmp[5]
+        #     item_name = inputdata[0]
+        #     expire_date = inputdata[1]
+        #     print(item_name,expire_date)
         if inputdata[0] == '/help' :
             replyBtns = [
                     [Btn(text='/show')],
-                    [Btn(text='/updateInfo'), Btn(text='/delete')],
+                    [Btn(text='/updateInfo')],
                     [Btn(text='/Immediate item'), Btn(text='/Expiring item')]
                 ]
-            time.sleep(1)
             bot.sendMessage(chat_id, 'Here\'s all function buttons', reply_markup=ReplyMarkup(keyboard=replyBtns))
-
         elif inputdata[0] == '/show' :
-            test = ''
-            test = getItemList()
-            print(test)
-            bot.sendMessage(chat_id, test)
+            show = ''
+            show = getItemList()
+            bot.sendMessage(chat_id, show)
         elif inputdata[0] == '/updateInfo' :
-            # barcode is illegal
-            #if db.checkdata(inputdata[2]) == 1
-                #bot.sendMessage(chat_id, 'please input itemname and expirationDate')
-            
+            # qrcode is illegal
             if len(inputdata)==4 :
-                bot.sendMessage(chat_id, 'please input itemname and expirationDate')
-                #/ asdasd  
-                #if inputdata[3].lower()[-1] =='h':
-                #h = 9H
-                #bot.sendMessage(chat_id, 'tmp')
-                #elif inputdata[3].lower()[-1] =='d':
-                #d
-                #elif inputdata[3].lower()[-1] =='y':
-                #y
-                #db.read_specified_data_use_serial_number(inputdata[2])[0][5]
-                #result = time.strftime('%Y-%m-%d %H:%M:%S', t)
-                #t = datetime.timedelta(hours=h , days= d, weeks=y)
-                #db.update_data_use_serial_number(5,t,)
-                #datetime.datetime
-                #9h/H 10d/D 1y/Y
-                
-                # 需要補判斷輸入的日期型態
+                date = inputdata[3].split('/')
+                if len(date)==4 :
+                    t = datetime.datetime(year=int(date[0]), month = int(date[1]), day = int(date[2]), hour = int(date[3]))
+                    updateItem(inputdata[1],inputdata[2],t)
+                    bot.sendMessage(chat_id,  msg['text'] + '\n' + 'Success!')
+                elif len(date)==3 :
+                    t = datetime.datetime(year=int(date[0]), month = int(date[1]), day = int(date[2]))
+                    updateItem(inputdata[1],inputdata[2],t)
+                    bot.sendMessage(chat_id,  msg['text']+ '\n' + 'Success!')
+                else :
+                    bot.sendMessage(chat_id, 'Usage:' + '\n' + '/updateInfo <qrcode> <item_name> <Year/Month/Day/Hour>' + '\n' + 'Example:' + '\n'+ '/updateInfo AXXXXX apple 2021/8/22/17')
             else: 
-                bot.sendMessage(chat_id, 'please input itemname and expirationDate')
-        elif inputdata[0] == '/add'  :
+                bot.sendMessage(chat_id, 'Usage:' + '\n' + '/updateInfo <qrcode> <item_name> <Year/Month/Day/Hour>' + '\n' + 'Example:' + '\n'+ '/updateInfo AXXXXX apple 2021/8/22/17')
+        elif inputdata[0] == '/Immediate item'  :
             bot.sendMessage(chat_id, '')
+        elif inputdata[0] == '/Expiring item'  :
+            bot.sendMessage(chat_id, '')
+
 
 if __name__ == '__main__':
     
@@ -108,20 +112,15 @@ if __name__ == '__main__':
     bot = telepot.Bot(TELEGRAM_BOT_TOKEN)
     print("I'm listening...")
     testurl = "\\villagerlimits.PNG"
-    barcode = 'test0999'
-    chat_id_list = {'217724690'} #'781745255',
-    #getNewItem(chat_id_list,barcode,testurl)
-    #takeOffItem(chat_id_list,barcode,testurl)
+    qrcode = 'test0999'
+    chat_id_list = [] #'217724690','781745255',
+    
+    #getNewItem(chat_id_list,qrcode,testurl)
+    #takeOffItem(chat_id_list,qrcode,testurl)
     # global variables
     itemString = ''
     totalList = []
     MessageLoop(bot,on_chat_message).run_as_thread()
     while 1:
         time.sleep(10)
-
-
-
-
-
-
 
